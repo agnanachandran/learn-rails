2c282749e2206e6d5ed75ee074d21b8f5bad0b15
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-02-14 22:04:14 -0500

    Add contact MVC and store info in GDrive

---
+++ app/models/contact.rb
@@ -0,0 +1,34 @@
+class Contact < ActiveRecord::Base
+  has_no_table
+
+  column :name, :string
+  column :email, :string
+  column :content, :string
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_presence_of :content
+  validates_format_of :email,
+    :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates_length_of :content, :maximum => 500
+
+  def update_spreadsheet
+    connection = GoogleDrive.login(ENV["GMAIL_USERNAME"], ENV["GMAIL_PASSWORD"])
+    ss = connection.spreadsheet_by_title('Learn-Rails-Example')
+    if ss.nil?
+      ss = connection.create_spreadsheet('Learn-Rails-Example')
+    end
+
+    # Insert our properties into the spreadsheet's 0th worksheet at the last row. Rows and columns are 1-based
+    ws = ss.worksheets[0]
+    last_row = 1 + ws.num_rows
+    # Use the format: ws[row_number, column_number]] 
+    ws[last_row, 1] = Time.new
+    ws[last_row, 2] = self.name
+    ws[last_row, 3] = self.email
+    ws[last_row, 4] = self.content
+    ws.save
+  end
+
+end
+
